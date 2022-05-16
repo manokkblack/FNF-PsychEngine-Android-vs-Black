26207afff96b4a32094c93bafefce3585341737d
manokkblack <103008983+manokkblack@users.noreply.github.com>
2022-05-16 17:21:16 -0300

    Update GraphicsSettingsSubState.hx

--- source/options/GraphicsSettingsSubState.hx
+++ source/options/GraphicsSettingsSubState.hx
@@ -33,19 +33,19 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 {
 	public function new()
 	{
-		title = 'Graphics';
+		title = 'Graficos';
 		rpcTitle = 'Graphics Settings Menu'; //for Discord Rich Presence
 
 		//I'd suggest using "Low Quality" as an example for making your own option since it is the simplest here
-		var option:Option = new Option('Low Quality', //Name
-			'If checked, disables some background details,\ndecreases loading times and improves performance.', //Description
+		var option:Option = new Option('graficos otimizados', //Name
+			'Se marcado\npode aumentar o desempenho, porém a qualidade dos graficos decairá', //Description
 			'lowQuality', //Save data variable name
 			'bool', //Variable type
 			false); //Default value
 		addOption(option);
 
 		var option:Option = new Option('Anti-Aliasing',
-			'If unchecked, disables anti-aliasing, increases performance\nat the cost of sharper visuals.',
+			'Se desmarcado, desativa o anti-aliasing, aumenta o desempenho\nao custo de visuais mais nítidos.',
 			'globalAntialiasing',
 			'bool',
 			true);
@@ -55,7 +55,7 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 
 		#if !html5 //Apparently other framerates isn't correctly supported on Browser? Probably it has some V-Sync shit enabled by default, idk
 		var option:Option = new Option('Framerate',
-			"Pretty self explanatory, isn't it?",
+			"Bem alto explicativo, certo?",
 			'framerate',
 			'int',
 			60);
